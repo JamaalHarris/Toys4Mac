e38a4c66014fe9a360d7ed72ebd513bab645afad
JamaalHarris <jamaalmalikharris@gmail.com>
2023-03-25 22:47:45 -0500

    reconfigures tests

--- test/controllers/toys_controller_test.rb
+++ test/controllers/toys_controller_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
 
 class ToysControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/toys.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Toy.count, data.length
+  end
+
+  test "create" do
+    assert_difference "Toy.count", 1 do
+      post "/toys.json", params: { name: "teddy bear", price: 80, image: "" }
+      assert_response 200
+    end
+  end
+
+  test "show" do
+    get "/toys/#{Toy.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["name", "price", "image", "created_at", "updated_at"], data.keys
+  end
+
+  test "update" do
+    toy = Toy.first
+    patch "/toys/#{toy.id}.json", params: { name: "Updated name" }
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal "Updated name", data["name"]
+  end
+
+  test "destroy" do
+    assert_difference "Toy.count", -1 do
+      delete "/toys/#{Toy.first.id}.json"
+      assert_response 200
+    end
+  end
 end
